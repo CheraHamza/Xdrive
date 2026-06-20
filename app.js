cdbbb11a0567b07d7d181ee6960193df9f253cfd
CheraHamza <cherahamza1@gmail.com>
2026-06-20 11:28:49 +0100

    Add live reload functionality and update dependencies

--- app.js
+++ app.js
@@ -5,10 +5,33 @@ import { router } from "./routes/router.js";
 import { sessionMiddleware } from "./config/passport.js";
 import passport from "passport";
 import "./config/passport.js";
+import dotenv from "dotenv";
+import livereload from "livereload";
+import connectLiveReload from "connect-livereload";
+
+dotenv.config();
 
 const app = express();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+if (process.env.NODE_ENV !== "production") {
+	const liveReloadServer = livereload.createServer({
+		exts: ["ejs", "html", "css", "js"],
+		port: 35729,
+	});
+
+	liveReloadServer.watch(path.join(__dirname, "views"));
+	liveReloadServer.watch(path.join(__dirname, "public"));
+
+	app.use(connectLiveReload({ port: 35729 }));
+
+	liveReloadServer.server.once("connection", () => {
+		setTimeout(() => {
+			liveReloadServer.refresh("/");
+		}, 100);
+	});
+}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
